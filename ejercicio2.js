f4936ddfcdfa3e51c384297d712d9b5a5ee03b14
erikag3798 <erikag3798@hotmail.com>
2022-06-21 15:15:51 +0200

    Se agrega el proyecto

---
+++ ejercicio2.js
@@ -0,0 +1,14 @@
+const lista= [
+    "Erika",
+    24,
+    false,
+    new Date(1998,6,03),
+    {
+       titulo:"Tus zonas erróneas",
+       autor:"Wayne W. Dyer",
+       fecha:new Date(1995,1,15),
+       url:"https://www.amazon.es/zonas-err%C3%B3neas-CLAVE-Wayne-Dyer/dp/8499085520"
+    }
+   
+   ]
+   console.log(lista)
